c7ad3e43967271e3ec34b2a501f93685d52bbf04
AMP <msprexy1@gmail.com>
2025-11-16 17:58:01 +0100

    Change function declarations in crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,7 +1,8 @@
 // task_3/js/crud.d.ts
 import { RowID, RowElement } from './interface';
 
-// Declare the functions exported from crud.js
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
+
+export { insertRow, deleteRow, updateRow };
